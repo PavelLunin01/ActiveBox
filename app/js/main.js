01c8ab6874a9fc6bf9047ce1bb44de7cc89d75be
PavelLunin01 <luninpavel01@gmail.com>
2021-12-28 00:25:24 +0300

    added testimonials, download, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-  
+  $('.testimonials__items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true
+  });
 });
