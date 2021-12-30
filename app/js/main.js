4dea60ce82b354bd8859716804eeea7d65641e75
PavelLunin01 <luninpavel01@gmail.com>
2021-12-30 17:39:02 +0300

    added burger menu, smooth scroll

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,54 @@ $(function(){
     arrows: false,
     dots: true
   });
+
+  let header = $("#header");
+  let intro = $("#intro");
+  let introHeight = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
+
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
+
+  checkScroll(scrollPos, introHeight);
+
+    $(window).on("scroll resize", function(){
+
+      introHeight = intro.innerHeight();
+      scrollPos = $(this).scrollTop();
+      checkScroll(scrollPos, introHeight);
+
+      nav.removeClass("show");
+
+    });
+
+  function checkScroll(scrollPos, introHeight){
+    if(scrollPos > introHeight){
+      header.addClass("fixed");
+    }
+    else{
+      header.removeClass("fixed");
+    }
+  }
+
+  navToggle.on("click", function(event){
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+  $("[data-scroll]").on("click", function(event){
+    event.preventDefault();
+
+    let elementId = $(this).data("scroll");
+
+    let elementOffset = $(elementId).offset().top;
+
+    $("html, body").animate({
+      scrollTop: elementOffset - 66
+    }, 700);
+
+    nav.removeClass("show");
+  });
+  
 });
